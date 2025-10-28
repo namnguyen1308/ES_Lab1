1f137f0b11cf79395de824031b7de817c31891c4
NAMNGUYEN\NAM NGUYEN <nam.nguyen1308hcmut@hcmut.edu.vn>
2025-10-29 02:02:31 +0700

    no message

---
+++ ex4/Core/Src/software_timer.c
@@ -0,0 +1,61 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: Sep 24, 2023
+ *      Author: HaHuyen
+ */
+
+#include "software_timer.h"
+
+#define TIMER_CYCLE_2 1
+#define COLON_CYCLE_2Hz (1000 / (2))
+
+//software timer variable
+uint16_t flag_timer2 = 0;
+uint16_t timer2_counter = 0;
+uint16_t timer2_MUL = 0;
+uint8_t colon_blink = 0;
+uint8_t colon_status = 0;
+/**
+  * @brief  Init timer interrupt
+  * @param  None
+  * @retval None
+  */
+void timer_init(){
+	HAL_TIM_Base_Start_IT(&htim2);
+}
+
+
+/**
+  * @brief  Set duration of software timer interrupt
+  * @param  duration Duration of software timer interrupt
+  * @retval None
+  */
+void setTimer2(uint16_t duration){
+	timer2_MUL = duration/TIMER_CYCLE_2;
+	timer2_counter = timer2_MUL;
+	flag_timer2 = 0;
+}
+
+/**
+  * @brief  Timer interrupt routine
+  * @param  htim TIM Base handle
+  * @note	This callback function is called by system
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+	if(htim->Instance == TIM2){
+		if(timer2_counter > 0){
+			timer2_counter--;
+			if(timer2_counter == 0) {
+				flag_timer2 = 1;
+				timer2_counter = timer2_MUL;
+			}
+		}
+		led7_Scan();
+		if(colon_blink == 0){
+			colon_status = !colon_status;
+			led7_SetColon(colon_status);
+		}
+	}
+}
