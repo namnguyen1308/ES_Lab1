a388d78692a03b3aca51c751b27236aae4850fb5
ryx <117896429+namnguyen1308@users.noreply.github.com>
2025-10-29 02:05:41 +0700

    Refactor system initialization and remove clock config
    Removed clock configuration code and added system initialization function.

--- ex1/Core/Src/main.c
+++ ex1/Core/Src/main.c
@@ -162,24 +162,7 @@ void SystemClock_Config(void) {
 	}
 
 	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
-
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
-		Error_Handler();
-	}
-}
-
-/* USER CODE BEGIN 4 */
-void init_system(void) {
-	timer2_init();
-	timer2_set(250);
-
+	 *frequency
 	led_7seg_init();
 }
 
